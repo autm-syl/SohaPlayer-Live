2ecacc5d5680de7b7ab523e8ea89cfdfad721a10
lecuong <cuongleviet@vccorp.vn>
2017-01-23 00:25:48 -0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-SohaPlayer-Live_Tests/Pods-SohaPlayer-Live_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SohaPlayer_Live_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SohaPlayer_Live_Tests
+@end
---
+++ Example/Pods/Target Support Files/SohaPlayer-Live/SohaPlayer-Live-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SohaPlayer_Live : NSObject
+@end
+@implementation PodsDummy_SohaPlayer_Live
+@end
